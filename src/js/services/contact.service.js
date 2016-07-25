efa116b8f32f29a581e00259742c58ed3b772bef
tdrobinson94 <tdrobinson94@gmail.com>
2016-07-25 16:18:43 -0400

    wrote out service methods

--- src/js/services/contact.service.js
+++ src/js/services/contact.service.js
@@ -1,11 +1,31 @@
 function ContactService(){
 
   this.createContact = createContact;
+  this.deleteContact = deleteContact;
+  this.readContact = readContact;
+  this.readContacts = readContacts;
+  this.updateContact = updateContact;
 
 
   function createContact(contact){
     return $http.post(SERVER.URL, contact);
   };
+
+  function readContact(id){
+    return $http.get(SERVER.URL + id);
+  };
+
+  function readContacts(){
+    return $http.get(SERVER.URL);
+  };
+
+  function updateContact(contact){
+    return $http.put(SERVER.URL + contact._id, contact);
+  };
+
+  function deleteContact(id){
+    return $http.delete(SERVER.URL + id);
+  }
 
 };
 

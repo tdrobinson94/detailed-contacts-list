12b641f47c170d08938856b849e387d7f5af507b
tdrobinson94 <tdrobinson94@gmail.com>
2016-07-25 15:24:39 -0400

    initial commit and config setup

---
+++ src/js/config.js
@@ -0,0 +1,29 @@
+function config($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state('root', {
+      templateUrl: 'templates/layout.tpl.html',
+      abstract: true
+    })
+
+    .state('root.home', {
+      url: '/',
+      templateUrl: 'templates/home.tpl.html'
+    })
+
+    .state('root.contact', {
+      url: '/contact',
+      templateUrl: 'templates/contact.tpl.html'
+    })
+
+    .state('root.add', {
+      url: '/add',
+      templateUrl: 'templates/add.tpl.hmtl'
+    })
+
+  $urlRouterProvider.otherwise('/');
+
+}
+
+config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+export {config};
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import angular from 'angular';
+import 'angular-ui-router';
+
+
+import {config} from './config';
+
+
+angular
+  .module('app', ['ui.router'])
+  .config(config)
+;
